fd2f5e9f3e0ed9dad2c78f8dd43ea285bf94281d
František Maša <frantisekmasa1@gmail.com>
2019-08-16 12:53:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Monolog ➜ Sentry breadcrumbs
+
+[![Build Status](https://travis-ci.org/fmasa/sentry-breadcrumbs-monolog-handler.svg?branch=master)](https://travis-ci.org/fmasa/sentry-breadcrumbs-monolog-handler)
+[![Coverage Status](https://coveralls.io/repos/github/fmasa/sentry-breadcrumbs-monolog-handler/badge.svg?branch=master)](https://coveralls.io/github/fmasa/sentry-breadcrumbs-monolog-handler?branch=master)
+
+
+## Installation
+The best way to install fmasa/sentry-breadcrumbs-monolog-handler is using [Composer](https://getcomposer.org/):
+
+    $ composer require fmasa/sentry-breadcrumbs-monolog-handler
---
+++ tests/BreadcrumbsHandlerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fmasa\SentryBreadcrumbsMonologHandler;
+
+use Monolog\Logger;
+use PHPUnit\Framework\TestCase;
+use Sentry\State\HubInterface;
+use Sentry\State\Scope;
+
+final class BreadcrumbsHandlerTest extends TestCase
+{
+    /** @var Scope */
+    private $scope;
+
+    /** @var BreadcrumbsHandler */
+    private $handler;
+
+    protected function setUp() : void
+    {
+        $this->scope = new Scope();
+
+        $hub = $this->createMock(HubInterface::class);
+        $hub->expects($this->once())
+            ->method('configureScope')
+            ->willReturnCallback(function (callable $callback) {
+                return $callback($this->scope);
+            });
+
+        $this->handler = new BreadcrumbsHandler($hub);
+    }
+
+    public function testWriteRecord() : void
+    {
+        $record = [
+            'message' => 'Test message',
+            'level' => Logger::WARNING,
+            'context' => ['option' => 'value'],
+            'extra' => [],
+        ];
+
+        $this->handler->handle($record);
+
+        $breadcrumbs = $this->scope->getBreadcrumbs();
+
+        $this->assertCount(1, $breadcrumbs);
+        $this->assertSame($record['message'], $breadcrumbs[0]->getMessage());
+        $this->assertSame($record['context'], $breadcrumbs[0]->getMetadata());
+        $this->assertSame('default', $breadcrumbs[0]->getType());
+    }
+}
